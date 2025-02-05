8c0fd34e2d3be2d005022e0b6b2e70e6357acf13
Musialkov <69191839+Musialkov@users.noreply.github.com>
2025-02-05 17:45:02 +0100

    add readmy file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # mat-simulator-task2
+
+This project implements a simple interactive mat simulator and extends it with circular pixel animations.
+
+## How to Run
+
+1. Ensure you have Node.js installed on your machine.
+2. Clone the repository and navigate to the main folder.
+3. Run the simulation using the following command:
+   `node . `
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import { CircleAnimation } from "./animations/circle-animation.js";
 import { MatSimulator } from "./simulator/mat-simulator.js";
 
-const circleAnimation = new CircleAnimation(7, 7, 7);
-const circleAnimation2 = new CircleAnimation(3, 8, 3);
+// You can adjust the size of the mat and the refresh interval in constants.js
+
+// Create a circle animation with parameters:
+// (x: 7, y: 7, radius: 3)
+// You can add multiple animations to the simulator.
+const circleAnimation = new CircleAnimation(7, 7, 3);
+
+//const circleAnimation2 = new CircleAnimation(7, 14, 3);
+
 const matSimulator = new MatSimulator();
 
 matSimulator.addAnimation(circleAnimation);
-matSimulator.addAnimation(circleAnimation2);
+//matSimulator.addAnimation(circleAnimation2);
 matSimulator.runSimulation();
